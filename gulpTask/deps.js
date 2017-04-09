aa96bcdfc6e677ae685e8352f8c3729cc6f7fdb1
Wilson Pereira Novaes Filho <wilsonnovaes00@hotmail.com>
2017-04-09 13:24:10 -0300

    Declaração de build

--- gulpTask/deps.js
+++ gulpTask/deps.js
@@ -1,20 +1,13 @@
-const gulp = require('gulp')
-const uglify = require('gulp-uglify')
-const uglifycss = require('gulp-uglifycss')
-const concat = require('gulp-concat')
+const gulp        = require('gulp')
+const uglify      = require('gulp-uglify')
+const uglifycss   = require('gulp-uglifycss')
+const concat      = require('gulp-concat')
 
 gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
 
 gulp.task('deps.js', function() {
   gulp.src([
-    'node_modules/angular/angular.min.js',
-    'node_modules/angular-ui-router/release/angular-ui-router.min.js',
-    'node_modules/angular-animate/angular-animate.min.js',
-    'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
-    'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
-    'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
-    'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
-    'node_modules/admin-lte/dist/js/app.min.js',
+    /*Arquivos de Javacripts*/
   ])
   .pipe(uglify())
   .pipe(concat('deps.min.js'))
@@ -23,11 +16,7 @@ gulp.task('deps.js', function() {
 
 gulp.task('deps.css', function() {
   gulp.src([
-    'node_modules/angular-toastr/dist/angular-toastr.min.css',
-    'node_modules/font-awesome/css/font-awesome.min.css',
-    'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
-    'node_modules/admin-lte/dist/css/AdminLTE.min.css',
-    'node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
+    /*Arquivos de Css*/
   ])
   .pipe(uglifycss({ "uglyComments": true }))
   .pipe(concat('deps.min.css'))
@@ -36,8 +25,7 @@ gulp.task('deps.css', function() {
 
 gulp.task('deps.fonts', function() {
   gulp.src([
-    'node_modules/font-awesome/fonts/*.*',
-    'node_modules/admin-lte/bootstrap/fonts/*.*'
+    /*Arquivos de Fonts*/
   ])
   .pipe(gulp.dest('public/assets/fonts'))
 })
